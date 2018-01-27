41fa3a581f7256c10d10fc819aba9358d64cc9cf
jxe <16373681+1ijack@users.noreply.github.com>
2018-01-27 15:54:41 -0800

    Report Cleanup
    - Adjusted stdout reporting to hopefully be more concise.
    - Utilizing window title for current status reporting
    - Removed old commented lines
    + Pre-clearing all used variables during init

--- ah.fu.cmd
+++ ah.fu.cmd
@@ -1,10 +1,10 @@
 :: By JaCk | Release 10/02/2017 | ah.fu.cmd  --  fu m$, untel and other asssholes
-@echo off & setlocal DisableDelayedExpansion EnableExtensions & title Killing em softly with this sung
+@echo off & setlocal DisableDelayedExpansion EnableExtensions & ((for /f "tokens=1 delims==" %%V in ('set afu_') do set "%%~V=" ) 2>nul 1>nul ) & title %~nx0: Killing em softly with this song
 
 ::: User Settings
 ::
 rem  semi-colon separated list of processes
- set "afu_process_list=igfxHK.exe;igfxTray.exe;atbroker.exe;tabtip.exe"
+ set "afu_process_list=igfxEM.exe;igfxHK.exe;igfxTray.exe;atbroker.exe;tabtip.exe"
 
 ::: Script Settings
 ::
@@ -12,8 +12,8 @@ rem  semi-colon separated list of processes
 rem  dynamic, local file with a process list ---- conf syntax - a list (space,comma,semi-colon delimited)
  rem example:  (echo/afu_process_list=igfxEM.exe,igfxHK.exe,igfxTray.exe,atbroker.exe,tabclip.exe,adminservice.exe,PresentationFontCache.exe,SMSvcHost.exe,WUDFHost.exe) 1> "%afu_list_conf%"
  set "afu_list_conf=%userprofile%\eph.u.conf"
- set "afu_list_add_unvarred_keys=true"       rem Set to [true]  if you want to have afu_list_conf add keys as processes; 
-    rem Example:
+ set "afu_list_add_unvarred_keys=true"       rem Set to [true]  if you want to have [afu_list_conf] add alienated keys as processnames to watch; 
+    rem Example when true:  $0  would see "mydumb.exe" and "other dump.exe" as processnames, but still see "afu_process_list" as a list label
     rem  afu_process_list=igfxEM.exe,"other with spaces.exe"
     rem 
     rem mydumb.exe
@@ -34,28 +34,28 @@ rem  default: 3 --  integer, check conf for changes every after X tasklist check
  set "afu_cntr=0"
  set "afu_chkcntr=0"
  set "afu_killcntr=0"
+ if not defined afu_tcntdwn_secs set "afu_tcntdwn_secs=360"
  if not defined afu_cycle_check_conf set "afu_cycle_check_conf=3"
-  if defined afu_list_conf (
-    call :func_afu_notifly watching for changes: "%afu_list_conf%"
-    call :func_afu_notifly Check conf every %afu_cycle_check_conf% cycles of %afu_tcntdwn_secs%secs
- )
+ 
+ if defined afu_list_conf call :func_afu_notifly Conf: Watching for changes [%%afu_cycle_^check_conf%%x%%afu_tcnt^dwn_secs%%secs]: "%afu_list_conf%"
  goto :lbl_afu_inf_to_inf
 exit /b
  
 
-:::::::::::::::::::
-::               ::
-::   Bunctions   ::
-::               ::
-:::::::::::::::::::
-goto :eof
+:::::::::::::::::::::::::::::::::::::::
+::                                   ::
+::   Bunctions                       ::
+::                                   ::
+:::::::::::::::::::::::::::::::::::::::
+                              goto :eof
 
 
-:: Do you like hampster-wheels?
+:: Do you like hampster-wheels?  Introducing the mein hampster
 :lbl_afu_inf_to_inf
     if defined afu_debug_dump echo/debug: %~0 %*
     
     rem  checks conf every cycle reset
+    call :func_afu_titlifli Status: Checking Conf: "%afu_list_conf%"
     if defined afu_list_conf call :func_afu_process_conf_file "%afu_list_conf%" "afu_process_list"
     
     for /l %%T in (1,1,%afu_cycle_check_conf%) do (
@@ -68,16 +68,18 @@ goto :eof
 goto :eof
 
 
-:: buntchler -- the task butcher
+:: butchler -- the task butcher
+rem  'He's the "clean-up" man.  The best in town.'
 :func_afu_send_eph_uoo
     if defined afu_debug_dump echo/debug: %~0 %*
     
-    rem  for live debug dumps toggle
+    rem live debug dumps toggle -- [un]comment line below during script execution to toggle debug dumps
     REM set "afu_debug_dump=true"
-    
-    call :func_afu_titlifli
+
+    call :func_afu_titlifli Status: Checking process tasklist
     for /f "tokens=1,* delims==" %%Y in ('set %~1') do for %%A in (%%Z) do (
         set /a "afu_chkcntr+=1"
+        call :func_afu_titlifli Status: Checking process tasklist: [%%afu_^chkcntr%%]: %%~A
         
        if defined afu_debug_dump echo/debug: taskkill /f /im "%%~A"
         
@@ -86,12 +88,15 @@ goto :eof
             for /f "tokens=3,*" %%H in ("%%T") do call :func_afu_notifly %%I
         )
     )
+    call :func_afu_titlifli Status: idle
         
 goto :eof
+
 
 ::  Usage  ::  func_afu_process_conf_file    "%afu_list_conf%"   "afu_process_list"
 :: Process local process list file
 rem   leave ASAP when file size hasn't changed
+rem   Note: when multiple updates from conf occur, notify may notify 
 :func_afu_process_conf_file
     if defined afu_debug_dump echo/debug: %~0 %*
     if  "%~1"   equ   ""  exit /b 456
@@ -106,36 +111,31 @@ rem   leave ASAP when file size hasn't changed
             
         ) else if /i "%%~A" equ "afu_cycle_check_conf" (
             if 9%%~B neq 9%afu_cycle_check_conf% (
-                call :func_afu_notifly Update: Conf: Check: Cycles: To: %%~B; Prev: %afu_cycle_check_conf%;
-                call :func_afu_notifly Check conf every %%~B cycles of %afu_tcntdwn_secs%secs
+                call :func_afu_notifly Update: CheckConf: [%%~Bx%%afu_tcnt^dwn_secs%%secs]: Number of Cycles New: %%~B;  [Prev: %afu_cycle_check_conf%]
                 set "afu_cycle_check_conf=%%~B"
             )
         ) else if /i "%%~A" equ "afu_tcntdwn_secs" (
             if 9%%~B neq 9%afu_tcntdwn_secs% (
-                call :func_afu_notifly Update: Timer: Wait: ProcessCheck: To: %%~B; Prev: %afu_tcntdwn_secs%;
-                call :func_afu_notifly Check conf every %afu_cycle_check_conf% cycles of %%~Bsecs
+                call :func_afu_notifly Update: CheckConf: [%%afu_cycle_^check_conf%%x%%~Bsecs]: Wait cycle time:  New: %%~B;  [Prev: %afu_tcntdwn_secs%]
                 set "afu_tcntdwn_secs=%%~B"
             )
 
         ) else if "%%~B" neq "" (
             for %%P in (;%%B;) do call :func_afu_conf_condem "%%~A" %%P  "%~2"  "notify" 
         
-        ) else if "%%~xA" neq "" call :func_afu_conf_condem   ""    %%A  "%~2"  "notify"
+        ) else if "%%~xA" neq "" (
+               call :func_afu_conf_condem   ""    %%A  "%~2"  "notify"
+
+        ) else call :func_afu_conf_condem   ""    %%A  "%~2"  "notify"
     )
     set "fsize=%~z1"
     
 goto :eof
 
-REM ) else if "%%~B" neq "" (
-        REM for %%P in (;%%B;) do if "%%~P" neq "" for /f "tokens=1,* delims==" %%Y in ('set %~2') do for %%L in (%%Z) do if "%%~P" neq "%%~L" call :func_afu_add_to_list "%~2"  %%P  "notify" 
-REM ) else if "%%~xA" neq "" (
-        REM for %%P in (;%%A;) do if "%%~P" neq "" for /f "tokens=1,* delims==" %%Y in ('set %~2') do for %%L in (%%Z) do if "%%~P" neq "%%~L" call :func_afu_add_to_list "%~2"  %%P   "notify"
-
 
 ::  Usage  ::  func_afu_conf_condem    "keyName-Optional"  "proccessName"  "afu_process_list"  VarGroup
 :func_afu_conf_condem
     if defined afu_debug_dump echo/debug: %~0 %*
-    
     for /f "tokens=1,* delims==" %%Y in ('set %~3') do for %%L in (%%Z) do if "%~2" neq "%%~L" call :func_afu_add_to_list "%~3" "%~2" "%~4" "%~1"
 goto :eof
 
@@ -158,10 +158,12 @@ rem  Adds only unique strings into list
     for /f "tokens=1,* delims==" %%a in ('set %~1') do for %%L in (%%b) do if %%L equ "%~2" goto :eof
     for /f "tokens=1,* delims==" %%a in ('set %~1') do set "%~1=%%b;%2"
 
-    rem hearhe hearhe
-    if /i "%~3" equ "notify" call :func_afu_notifly Added to Kill list: %~4 %2
+    if /i "%~3" neq "notify" goto :eof   rem reporting -- hearhe hearhe
+    if "%~4" equ "" call :func_afu_notifly Added to Kill list: %2 &goto :eof
+    call :func_afu_notifly Added to Kill list: %~4: %2
 
 goto :eof
+
 
 ::  Usage  ::  func_afu_sub_of_list  listVar  "String to remove"  "notifyUserBool" VarGroup
 rem  Removes a unique string from list
@@ -196,13 +198,14 @@ rem  Removes a unique string from list
 goto :eof
 
 
-::  Usage  :: func_afu_notifly  "stringsToPrintToConsole"  "moreStrings"
+::  Usage  :: func_afu_notifly  stringsToPrintToConsole with "qoStrings" and moStrings
 rem  Prints to Console with current count status and any addition strings from params
-rem  Note:
-rem    Total Killed:     %afu_killcntr%
-rem    Total Checked:    %afu_chkcntr%
-rem    Total Cycles:     %afu_cntr%
-rem    Total List Items: %afu_lstcntr%
+rem  Note: Prints params raw : so if you add quotes, will print with said quotes
+rem  Note: WTHeck Table:
+rem    Total Killed:     K:  %afu_killcntr%
+rem    Total Checked:    T:  %afu_chkcntr%
+rem    Total Cycles:     C:  %afu_cntr%
+rem    Total List Items: P:  %afu_lstcntr%
 :func_afu_notifly
     if defined afu_debug_dump echo/debug: %~0 %*
     call :func_afu_titlifli %*
@@ -210,44 +213,14 @@ rem    Total List Items: %afu_lstcntr%
 goto :eof
 
 
-::  Usage  :: func_afu_incrementor  "integer"  "var1" "var2" "var3" "var4" "etcORinf"
-rem  Increments the proceeding params by param1 OR 1 
-rem  Note:
-rem    When param1 is not integer, treats it as a string
-REM :func_afu_incrementor
-    REM if defined afu_debug_dump echo/debug: %~0 %*
-    REM if "%~1" equ "" goto :eof
-    REM if "%~2" equ "" goto :eof
-    
-    REM if defined _afu_am_ for %%A in (%*) do "set /a %%~A+=%_afu_am_%"
-        REM set "_afu_am_="
-        REM goto :eof 
-    REM ) else set "_afu_am_=1"
-
-    REM ( set /a "_afu_am_+=( %~1 ) / 1"
-    REM ) 2>nul 1>nul
-    
-    REM if "%_afu_am_%" equ "0" set "_afu_am_=" &goto :eof
-    
-    REM if "%~1" equ "%_afu_am_%" shift \1
-        REM call %~0 %
-    
-    
-        REM for %%A in (%*) do "set /a %%~A+=%_afu_am_%"
-        
-    
-    
-    REM call :func_afu_titlifli %*
-    REM echo/ %~nx0: %date:~4,-5% %time:~0,-3% [%afu_lstcntr%P][%afu_killcntr%K/%afu_chkcntr%T:%afu_cntr%C]: %*
-REM goto :eof
-
-::  Usage  :: func_afu_titlifli  "stringsToPrintToTitleBar"  "moreStrings"
+::  Usage  :: func_afu_titlifli  stringsToPrintToConsole with "qoStrings" and moStrings
 rem  Sets Title with current count status and any addition strings from params
-rem  Note:
-rem    Total Killed:     %afu_killcntr%
-rem    Total Checked:    %afu_chkcntr%
-rem    Total Cycles:     %afu_cntr%
-rem    Total List Items: %afu_lstcntr%
+rem  Note: displays params raw : so if you add quotes, will display with said quotes
+rem  Note: WTHeck Table:
+rem    Total Killed:     K:  %afu_killcntr%
+rem    Total Checked:    T:  %afu_chkcntr%
+rem    Total Cycles:     C:  %afu_cntr%
+rem    Total List Items: P:  %afu_lstcntr%
 :func_afu_titlifli
     if defined afu_debug_dump echo/debug: %~0 %*
 
@@ -258,6 +231,7 @@ goto :eof
 
 
 :: outliketrout
+rem  I gots me a tacklebox and fishing rod but no cycletime to fish.....
 :func_afu_trout_fishing
     if defined afu_debug_dump echo/debug: %~0 %*
     set "afu_process_list="
