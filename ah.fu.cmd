38d3f79a6ebac4fc77192e0299406eed7963cfcc
jxe <16373681+1ijack@users.noreply.github.com>
2018-02-05 11:56:10 -0800

    Self Elevate, Cleanup, FishingYes
    - Added Self Elevate (as admin.  Requires admin access) when boolean(s) for elevation are set/found
    - Cleaned up comments to further explain things in a more concise fashion
    - Fishing - pun/joke on function "outliketrout" - there hasn't been a real reason to cleanly exit the script, (although the functions were created to do so).  Now with elevate changes, af.fu.cmd can take cmd of the fishing rod and reel in the catch while driving into the sunset.

--- ah.fu.cmd
+++ ah.fu.cmd
@@ -1,36 +1,43 @@
 :: By JaCk | Release 10/02/2017 | ah.fu.cmd  --  fu m$, untel and other asssholes
 @echo off & setlocal DisableDelayedExpansion EnableExtensions & ((for /f "tokens=1 delims==" %%V in ('set afu_') do set "%%~V=" ) 2>nul 1>nul ) & title %~nx0: Killing em softly with this song
 
-::: User Settings
+::::  User Settings
 ::
-rem  semi-colon separated list of processes
- set "afu_process_list=igfxEM.exe;igfxHK.exe;igfxTray.exe;atbroker.exe;tabtip.exe"
+set "afu_process_list=igfxEM.exe;igfxHK.exe;igfxTray.exe;atbroker.exe;tabtip.exe"   rem  semi-colon separated list of processes
 
-::: Script Settings
+::::  Script Settings
 ::
- set "afu_debug_dump="   rem set to anything to see bunction and it's params
-rem  dynamic, local file with a process list ---- conf syntax - a list (space,comma,semi-colon delimited)
+rem  External - live config: dynamic, local file with a process list ---- conf syntax - a list (space,comma,semi-colon delimited)
  rem example:  (echo/afu_process_list=igfxEM.exe,igfxHK.exe,igfxTray.exe,atbroker.exe,tabclip.exe,adminservice.exe,PresentationFontCache.exe,SMSvcHost.exe,WUDFHost.exe) 1> "%afu_list_conf%"
  set "afu_list_conf=%userprofile%\eph.u.conf"
- set "afu_list_add_unvarred_keys=true"       rem Set to [true]  if you want to have [afu_list_conf] add alienated keys as processnames to watch; 
-    rem Example when true:  $0  would see "mydumb.exe" and "other dump.exe" as processnames, but still see "afu_process_list" as a list label
-    rem  afu_process_list=igfxEM.exe,"other with spaces.exe"
-    rem 
-    rem mydumb.exe
-    rem "other dump.exe"
- 
-rem  wait to kill again; value in seconds; default: 360
- set "afu_tcntdwn_secs=360"
- set "afu_tcntdwn_secs=60"
+
+   ::   Example Config:  
+   ::       $0  would see "mydumb.exe" and "other dump.exe" as processnames, but still see "afu_process_list" as a list label
+   ::    
+   ::    kill_list = igfxEM.exe ; "other with spaces.exe" ; a.out 
+   ::    mydumb.exe
+   ::    "other dump.exe"
+    
+rem  Treat isolated strings as processnames -- default: true
+ set "afu_list_add_unvarred_keys=true"     rem  -- Set to [true]  if you want to have [afu_list_conf] add alienated keys as processnames to watch; 
 
 rem  default: 3 --  integer, check conf for changes every after X tasklist checks
  set "afu_cycle_check_conf=5"
 
-  
+rem  Admin/Debug Bools 
+ set "afu_sudo_admin_root_plus_ultra="     rem  -- Relaunch self in elevated env when not elevated --  true/undefined  --  although awesome, hard to remember
+ set "afu_SanSudoSue="                     rem  -- Relaunch self in elevated env when not elevated --  true/undefined  -- ConfigFriendlier Name
+ set "afu_debug_dump="                     rem  -- Dump More Msgs - print internal keys and values --  set to anything to see bunction and it's params
+
+rem  wait to kill again; value in seconds; default: 360
+ set "afu_tcntdwn_secs=360"
+ set "afu_tcntdwn_secs=60"
  
 ::: GOGOGOOGOG
 :: fun in circles 
 ::
+ call :func_afu_adminCheck_net_session afu_shkala_su
+ if not defined afu_shkala_su if defined afu_sudo_admin_root_plus_ultra call :func_afu_can_i_has_admin afu_shkala_su  "%temp%\sudo_make_me_a_sandwich.vbs"  afu_tacklebox
  set "afu_cntr=0"
  set "afu_chkcntr=0"
  set "afu_killcntr=0"
@@ -54,12 +61,16 @@ exit /b
 :lbl_afu_inf_to_inf
     if defined afu_debug_dump echo/debug: %~0 %*
     
+    rem  finally, a reason
+    if defined afu_tacklebox goto :func_afu_trout_fishing
+    
     rem  checks conf every cycle reset
     call :func_afu_titlifli Status: Checking Conf: "%afu_list_conf%"
     if defined afu_list_conf call :func_afu_process_conf_file "%afu_list_conf%" "afu_process_list"
     
     for /l %%T in (1,1,%afu_cycle_check_conf%) do (
         set /a "afu_cntr+=1"
+        if defined afu_tacklebox goto :func_afu_trout_fishing
         call :func_afu_send_eph_uoo "afu_process_list" 2>nul
         ( call timeout /t %afu_tcntdwn_secs% ) 2>nul 1>nul
     )
@@ -119,7 +130,16 @@ rem   Note: when multiple updates from conf occur, notify may notify
                 call :func_afu_notifly Update: CheckConf: [%%afu_cycle_^check_conf%%x%%~Bsecs]: Wait cycle time:  New: %%~B;  [Prev: %afu_tcntdwn_secs%]
                 set "afu_tcntdwn_secs=%%~B"
             )
-
+        ) else if /i "%%~A" equ "afu_sudo_admin_root_plus_ultra" (
+            if not defined afu_shkala_su ( 
+                call :func_afu_can_i_has_admin afu_shkala_su  "%temp%\sudo_make_me_a_sandwich.vbs"   afu_tacklebox
+                goto :eof
+            )
+        ) else if /i "%%~A" equ "afu_SanSudoSue" (
+            if not defined afu_shkala_su ( 
+                call :func_afu_can_i_has_admin afu_shkala_su  "%temp%\sudo_make_me_a_sandwich.vbs"   afu_tacklebox
+                goto :eof
+            )
         ) else if "%%~B" neq "" (
             for %%P in (;%%B;) do call :func_afu_conf_condem "%%~A" %%P  "%~2"  "notify" 
         
@@ -227,6 +247,34 @@ rem    Total List Items: P:  %afu_lstcntr%
     set "afu_lstcntr="
     for %%x in (%afu_process_list%) do set /a "afu_lstcntr+=1"
     title %~nx0: %date:~4,-5% %time:~0,-3% [%afu_lstcntr%P][%afu_killcntr%K/%afu_chkcntr%T:%afu_cntr%C]: %*
+goto :eof
+
+
+::  Usage  ::  func_afu_can_i_has_admin   returnVar   "path\file.name.vbs"   "afu_tacklebox"
+rem  Elevates this script with administrator privledges -- param1: returnVar with true/undefined; -- param2: scriptname
+:func_afu_can_i_has_admin
+    if "%~1" equ "" ( goto :eof ) else ( ( if "%~2" equ "" ( goto :eof ) else if "%~x2" neq ".vbs" call %~0  %1  "%~2.vbs"  &goto :eof ) & set "%~1=" )
+    if "%~3" equ "afu_tacklebox" set "afu_tacklebox=true"
+    if not defined %~1 call :func_afu_adminCheck_net_session  %1
+    if not defined %~1 call :func_afu_create_elevator  %2
+    if exist %2 "%SYSTEMROOT%\system32\cscript.exe" //nologo //b %2 "%~f0"
+    ( ( timeout /t 2 ) & del /q %2 
+    ) 2>nul 1>nul 
+goto :eof
+
+::  Usage  ::  func_afu_create_elevator   path\file.name.vbs
+rem  Creates a vbs script to param1, or "%temp%\getadmin.vbs". Note: extension needs to be .vbs
+:func_afu_create_elevator
+    if "%~1" equ "" ( call %~0  "%temp%\getadmin.vbs" &goto :eof ) else ( if "%~x1" neq ".vbs" ( call %~0  "%~1.vbs" &goto :eof ))
+    ( echo/&echo/Set Admin = CreateObject^("Shell.Application"^)
+      echo/Admin.ShellExecute WScript.Arguments^(0^), "", "", "runas", 1) > "%~1"
+goto :eof
+
+::  Usage  ::  func_afu_adminCheck_net_session  returnVar
+rem  Returns (Err0) param1 set to true when elevated, otherwise undefines param1 (Err2)
+:func_afu_adminCheck_net_session
+    if "%~1" neq "" ( set "%~1=true" ) else goto :eof
+    for /f "tokens=3" %%A in ('net session 2^>^&1') do if /i "%%~A" equ "denied." set "%~1="
 goto :eof
 
 
