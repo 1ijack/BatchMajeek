74583611fc80608eb1aacfcbd8a2a6c61ee98eb3
jxe <16373681+1ijack@users.noreply.github.com>
2018-04-25 00:31:48 -0700

    unixTime cleanup
    - Removed old code
    + Added more remarks into the user settings
    + added hardcoded timezone variable
    * moved debug msgs into it's own function

--- unixTime.bat
+++ unixTime.bat
@@ -16,13 +16,34 @@
 ::                                    ::
 ::::::::::::::::::::::::::::::::::::::::
 :func_utx_user_settings
+    rem ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    rem  -----------------------------------------
     rem  Boolean settings below, use the following values to enable/disable behavior: 
     rem    to Enable,  set to: true
     rem    to Disable, set to: false [or undefined]
+    rem  -----------------------------------------
 
     rem  Note:  When below setting is undefined/false, output is seconds;  otherwise, when value is 'true', output is in milliseconds
     set "utx_output_milliseconds="      rem  Default:      ;  Description: utx_output_milliseconds --- will display with ms upto 1000 ms
+    rem  -----------------------------------------
+    rem ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+    
+    rem ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    rem -----------------------------------------
+    rem  String settings below, use expected strings
+    rem    to Enable,  set to: <string>
+    rem    to Disable, set to: [undefined]
+    rem -----------------------------------------
+
+    rem  The following is to bypass asking the w32tm.exe for the timezone settings.  Value should be in seconds (integer).  When value is undefined, script will ask the OS for timezone configuration.
+    rem  -- Example: UTC+7 hours  (aka 420 min), value would be: 25200
+    rem  -- Example: UTC+0 hours  (aka   0 min), value would be: 0
+    rem  -- Example: UTC-10 hours (aka 600 min), value would be: -36000
+    set "utx_utc_offset_hardcoded="
+    rem -----------------------------------------
+    rem ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    
 goto :eof
 
 
@@ -33,13 +54,17 @@ goto :eof
 ::::::::::::::::::::::::::::::::::::::::
 :func_utx_main
     call :func_utx_user_settings
+    call :func_utx_error_checks
     call :func_utx_calc_date_secs   "utx_time{unix}"
-    
+
     echo/%utx_time{unix}%
 
-::  Env Cleanup 
-( for /f "tokens=1 delims==" %%A in ('set utx_') do set "%%~A=" ) 2>nul 1>nul & ( endlocal & goto :eof )
+    rem Env Cleanup 
+    (   for /f "tokens=1 delims==" %%A in ('set utx_') do set "%%~A=" 
+        endlocal & goto :eof
+    ) 2>nul 1>nul 
 
+    exit /b 0
 goto :eof
 
 
@@ -49,6 +74,19 @@ goto :eof
 ::                                    ::
 ::::::::::::::::::::::::::::::::::::::::
 2>nul (  goto :eof  ||  exit /b  ) 1>nul
+
+::  Usage  ::   func_utx_error_checks
+rem  ensure that dependencies are resolved
+:func_utx_error_checks
+    rem  boolean NEEDS to be defined as something
+    if "%utx_output_milliseconds%" equ "" set "utx_output_milliseconds=false"
+
+    rem  Making sure binary exists, hard-coded value takes priority
+    if "%utx_utc_offset_hardcoded%" neq "" (
+        set "utx_utc{shift}=%utx_utc_offset_hardcoded%"
+    ) else if not exist "%SystemRoot%\System32\w32tm.exe" set "utx_utc{shift}=0"
+    
+goto :eof
 
 
 ::  Usage  ::  func_utx_calc_time_secs_rounded   returnVarSecs   returnVarMS   "optional-HH:MM:SS.MS"
@@ -108,6 +146,12 @@ goto :eof
 ::  Usage  ::  func_utx_calc_time_secs_rounded   returnVar   "optional-DD/MM/YYYY"   "optional-DD/MM/YYYY"
 rem  calc days+months+years elapsed seconds (today/010111970 OR today/param3 OR param2/param3)
 rem  Note:  DOES NOT overwrite returnVar value.  Function ADDs to returnVar
+rem  Function Notes:
+rem    when param2 is greater than param3, swap variables;  compares YYYYMMDD
+rem    current time, timezone, DST and other adjustments
+rem    calculate all the leapyears between the two year ranges  --  using modulus,  adds '1' when leap-year --  highly modified from source: "http://www.robvanderwoude.com/datetiment.php#LeapYear"
+rem    Dates need to be defined as "MM/DD/YYYY" (strip DOW)
+rem    ghetto milliseconds ftw
 :func_utx_calc_date_secs
     if "%~1" equ "" goto :eof
     if "%~2" equ "" ( ( call %~0   %1   "%date:* =%"   %3 ) & goto :eof )
@@ -116,41 +160,39 @@ rem  Note:  DOES NOT overwrite returnVar value.  Function ADDs to returnVar
     set "utx_p{two}=%~2"
     set "utx_p{tri}=%~3"
     
-    rem  when param2 is greater than param3, swap variables;  compares YYYYMMDD
     if %utx_p{two}:~-4%%utx_p{two}:~-10,2%%utx_p{two}:~-7,2% lss %utx_p{tri}:~-4%%utx_p{tri}:~-10,2%%utx_p{tri}:~-7,2% (
-        set utx_p{two}=%utx_p{tri}%
-        set utx_p{tri}=%utx_p{two}%
+        set "utx_p{two}=%utx_p{tri}%"
+        set "utx_p{tri}=%utx_p{two}%"
     )
-
-    REM set /a "utx_leap{two}=((( %utx_p{two}:~-4% / 4 ) * 4 ) - %utx_p{two}:~-4% ) & ((( %utx_p{two}:~-4% / 100 ) * 100 ) - %utx_p{two}:~-4% ) & ((( %utx_p{two}:~-4% / 400 ) * 400 ) - %utx_p{two}:~-4% ), ! utx_leap{two}"
-    REM set /a "utx_leap{tri}=((( %utx_p{tri}:~-4% / 4 ) * 4 ) - %utx_p{tri}:~-4% ) & ((( %utx_p{tri}:~-4% / 100 ) * 100 ) - %utx_p{tri}:~-4% ) & ((( %utx_p{tri}:~-4% / 400 ) * 400 ) - %utx_p{tri}:~-4% ), ! utx_leap{tri}"
-
-    rem  using modulus,  returns 1 when leap-year, 0 when not --  highly modified from source: "http://www.robvanderwoude.com/datetiment.php#LeapYear"
-    REM set /a "utx_leap{two}=%utx_p{two}:~-4% % 4 & %utx_p{two}:~-4% % 100 & %utx_p{two}:~-4% % 400, ! %utx_leap{two}%"
-    REM set /a "utx_leap{tri}=%utx_p{tri}:~-4% % 4 & %utx_p{tri}:~-4% % 100 & %utx_p{tri}:~-4% % 400, ! %utx_leap{tri}%"
-    
-    REM call :func_utx_calc_time_secs_rounded   "utx_elapsed{secs}"  "%time%"
-    call :func_utx_calc_time_secs_ms        "utx_elapsed{secs}"   "utx_elapsed{ms}"   "%time%"
-    call :func_utx_get_utc_timeShift         "utx_utc{shift}"    "seconds"
-
     rem  not sure where my math is messed up, but the time is ahead 24 hours, hence making adjustment here
     set "utx_leap{total}=-1"
 
-    rem  calculate all the leapyears between the two year ranges  --  using modulus,  adds '1' when leap-year --  highly modified from source: "http://www.robvanderwoude.com/datetiment.php#LeapYear"
+    if not defined utx_utc{shift} call :func_utx_get_utc_timeShift   "utx_utc{shift}"    "seconds"
+    call :func_utx_calc_time_secs_ms   "utx_elapsed{secs}"   "utx_elapsed{ms}"   "%time%"
     for /l %%A in (%utx_p{tri}:~-4%,1,%utx_p{two}:~-4%) do set /a "utx_leap{total}+=( utx_leap{diff}=%%A %% 4 & %%A %% 100 & %%A %% 400, ! utx_leap{diff} )"
-    
-    rem   Dates need to be defined as "MM/DD/YYYY" (strip DOW)
+
     for /f "tokens=1-3,4-6 delims=-/" %%A in ("%utx_p{two}%/%utx_p{tri}%") do set /a "%~1=((((((( 1%%~A - 100 ) - ( 1%%~D - 100 )) * 304 ) + ((( 1%%~B - 100 ) - ( 1%%~E - 100 )) * 10  ) + (((  %%~C * 365 ) - (  %%~F * 365 )) * 10 )) / 10 ) + %utx_leap{total}% ) * 86400 ) + %utx_utc{shift}% + %utx_elapsed{secs}%"
-    if /i "%utx_output_milliseconds:~0,1%" equ "t" call set "%~1=%%%~1%%%utx_elapsed{ms}%"
 
+    if /i "%utx_output_milliseconds:~0,1%" neq "t" goto :eof
+    if defined utx_elapsed{ms} call set "%~1=%%%~1%%%utx_elapsed{ms}%"
+
+    REM call :func_utx_dumps_var_states
+    REM ( for %%A in (  "utx_leap{total}";  "utx_leap{diff}";  "utx_p{tri}";  "utx_p{two}"  ) do set "%%~A=" ) 2>nul 1>nul
+goto :eof
+
+
+:func_utx_dumps_var_states
     rem  Debug print statements: 
-    rem     if defined utx_elapsed{secs} echo utx_elapsed{secs} - %utx_elapsed{secs}%
-    rem     if defined utx_elapsed{ms}   echo utx_elapsed{ms}   - %utx_elapsed{ms}%
-    rem     if defined utx_utc{shift}    echo utx_utc{shift}    - %utx_utc{shift}%
-    rem     if defined utx_leap{total}   echo utx_leap{total}   - %utx_leap{total}%
-    rem     for /f "tokens=1-3,4-6 delims=-/" %%A in ("%utx_p{two}%/%utx_p{tri}%") do echo/A/B/C ^| D/E/F: %%A/%%B/%%C ^| %%D/%%E/%%F
-
-    ( for %%A in (  "utx_leap{total}";  "utx_leap{diff}";  "utx_leap{tri}";  "utx_leap{two}";  "utx_p{tri}";  "utx_p{two}"  ) do set "%%~A=" ) 2>nul 1>nul
+    if defined utx_elapsed{secs} echo/utx_elapsed{secs} - %utx_elapsed{secs}%
+    if defined utx_elapsed{ms}   echo/utx_elapsed{ms}   - %utx_elapsed{ms}%
+    if defined utx_leap{total}   echo/utx_leap{total}   - %utx_leap{total}%
+    if defined utx_utc{shift}    echo/utx_utc{shift}    - %utx_utc{shift}%
+    if defined utx_time{unix}    echo/utx_time{unix}    - %utx_time{unix}%
+    if defined utx_p{tri} if defined utx_p{two} (
+        echo/utx_p{two}        - %utx_p{two}%
+        echo/utx_p{tri}        - %utx_p{tri}%
+        for /f "tokens=1-3,4-6 delims=-/" %%A in ("%utx_p{two}%/%utx_p{tri}%") do echo/                   ^>  MM/DD/YYYY ^| MM/DD/YYYY &echo/                   ^>   A/ B/ C   ^|  D/ E/ F &echo/                   ^>  %%A/%%B/%%C ^| %%D/%%E/%%F 
+    )
 
 goto :eof
 
