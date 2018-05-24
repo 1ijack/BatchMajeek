39c36920020a738f40b42b8b86e9119b3c62e2bc
jxe <16373681+1ijack@users.noreply.github.com>
2018-05-23 20:27:49 -0700

    unixTime userEnv settings
    * Change user settings behavior so that pre-existing env variables for script behavior do not get cleared. allowing behavior changes on the fly OR predefined in the userEnv/sysEnv

--- unixTime.bat
+++ unixTime.bat
@@ -1,4 +1,4 @@
-::  by JaCk  |  Release 05/11/2018  |   https://github.com/1ijack/BatchMajeek/blob/master/unixTime.bat  |  unixTime.bat  --  returns current time in unixtime  --  total elapsed seconds since January 1st, 1970
+::  by JaCk  |  Release 05/23/2018  |   https://github.com/1ijack/BatchMajeek/blob/master/unixTime.bat  |  unixTime.bat  --  returns current time in unixtime  --  total elapsed seconds since January 1st, 1970
 :::
 :::  The zlib/libpng License -- https://opensource.org/licenses/Zlib
 ::  Copyright (c) 2018 JaCk
@@ -13,25 +13,26 @@
 ::  
 ::  3. This notice may not be removed or altered from any source distribution.
 :::
-@echo off & setlocal DisableDelayedExpansion EnableExtensions & for %%A in (utx_utc_offset;utx_out_ms;utx_leap{total};utx_utc{shift};utx_time{unix}) do set "%%A="
+@echo off & setlocal DisableDelayedExpansion EnableExtensions & for %%A in (utx_leap{total};utx_utc{shift};utx_time{unix};utx_out{bool}) do set "%%A="
 
 rem  Output seconds OR milliseconds.  
-set "utx_out_ms=false"
+if not defined utx_out_ms set "utx_out_ms="
   rem  set "utx_out_ms="            rem  output seconds
   rem  set "utx_out_ms=false"       rem  output seconds
   rem  set "utx_out_ms=true"        rem  output milliseconds
 
 rem  Bypass timezone check by forcing a manual offset
-set "utx_utc_offset="
+if not defined utx_utc_offset set "utx_utc_offset="
   rem  set "utx_utc_offset=25200"      rem  UTC+7  hours, (is  [420] min, is  [25200] seconds)
   rem  set "utx_utc_offset=0"          rem  UTC+0  hours, (is    [0] min, is      [0] seconds)
   rem  set "utx_utc_offset=-36000"     rem  UTC-10 hours, (is [-600] min, is [-36000] seconds)
 
 rem  main Calculation
 set /a "utx_utc{shift}=utx_utc_offset,utx_leap{total}=0"
-if not defined utx_out_ms set "utx_out_ms=false"
 if not defined utx_utc_offset ( for /f "tokens=1-8 delims=: " %%A in ('"%SystemRoot%\System32\w32tm.exe" /tz') do for %%e in ( "%%~A|%%~B"; "%%~B|%%~C"; "%%~C|%%~D"; "%%~D|%%~E"; "%%~E|%%~F"; "%%~F|%%~G"; "%%~G|%%~H" ) do for /f "tokens=1,2 delims=|" %%f in (%%e) do if /i "%%~f" equ "Bias" for /f "tokens=1 delims=min" %%m in ("%%~g") do if "%%~m" neq "0" set /a "utx_utc{shift}+=%%~m*60") 2>nul 1>nul
 for /l %%A in (1970,1,%date:~-4%) do set /a "utx_leap{total}+=!(%%A %% 4 & %%A %% 100 & %%A %% 400)"
 for /f "tokens=1-3,4-6 delims=-/" %%A in ("%date:* =%/01/01/1970") do set /a "utx_time{unix}=(((((1%%~A-1%%~D)*305)/10)+%utx_leap{total}%+(1%%~B-1%%~E)+(%%~C-%%~F)*365)*86400)-172800+%utx_utc{shift}%+((1%time:~-5,2%-100)+((1%time:~3,2%-100)*60)+(%time:~0,2%*3600))"
-(if /i "%utx_out_ms:~0,1%" equ "t" set "utx_time{unix}=%utx_time{unix}%%time:~-2%0") 2>&1
-(echo/%utx_time{unix}%)& for %%A in (utx_utc_offset;utx_out_ms;utx_leap{total};utx_utc{shift};utx_time{unix}) do set "%%A="
+set "utx_out{bool}=%utx_out_ms%false"
+if /i "%utx_out{bool}:~0,1%" equ "t" set "utx_time{unix}=%utx_time{unix}%%time:~-2%0"
+echo/%utx_time{unix}%
+for %%A in (utx_leap{total};utx_utc{shift};utx_time{unix};utx_out{bool}) do set "%%A="
